171d52b772fcdcfb3e809f36753cb654f6c63f66
Emil Bløcher <bloechrr@gmail.com>
2023-05-28 20:45:39 +0200

    add numbers

---
+++ numbers/numbers.java
@@ -0,0 +1,9 @@
+public class numbers {
+    public static void main (String[] ars){
+        int i;
+        int j = 42;
+        i = 0;
+        i++; //incrementering
+        System.out.println(i + j);
+    }
+}
